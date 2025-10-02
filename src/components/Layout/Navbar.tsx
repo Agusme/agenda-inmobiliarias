613a8b59d103b97a9f3c94a7fbdd1eb990714f2a
agusmena <agusmena60@mail.com>
2025-10-02 12:36:39 -0300

    fix: forzar tema claro en navegadores (deshabilitar dark mode y definir color-scheme light)

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
+import { navLinkClasses } from "../../utils/classNames";
 
 export default function Navbar() {
   return (
-    <div className="navbar shadow-sm bg-base-100">
+    <div className="navbar shadow-sm bg-gray-100">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -14,45 +15,27 @@ export default function Navbar() {
           </div>
           <ul tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-            <li><NavLink to="/" className={({ isActive }) =>
-              isActive ? "font-bold text-primary !bg-gray-50" : " font-semibold hover:text-primary hover:!bg-gray-100"
-            }>Inicio</NavLink></li>
-            <li><NavLink to="/clientes" className={({ isActive }) =>
-              isActive ? "font-bold text-primary !bg-gray-50" : " font-semibold hover:text-primary hover:!bg-gray-100"
-            }>Clientes</NavLink></li>
-            <li><NavLink to="/propiedades" className={({ isActive }) =>
-              isActive ? "font-bold text-primary !bg-gray-50" : " font-semibold hover:text-primary hover:!bg-gray-100"
-            }>Propiedades</NavLink></li>
-            <li><NavLink to="/propiedades-terceros" className={({ isActive }) =>
-              isActive ? "font-bold text-primary !bg-gray-50" : " font-semibold hover:text-primary hover:!bg-gray-100"
-            }>Propiedades Terceros</NavLink></li>
+            <li><NavLink to="/" className={navLinkClasses} >Inicio</NavLink></li>
+            <li><NavLink to="/clientes" className={navLinkClasses}>Clientes</NavLink></li>
+            <li><NavLink to="/propiedades" className={navLinkClasses} >Propiedades</NavLink></li>
+            <li><NavLink to="/propiedades-terceros" className={navLinkClasses}>Propiedades Terceros</NavLink></li>
           </ul>
         </div>
         <a className="btn btn-ghost normal-case text-2xl font-bold text-primary hover:!bg-gray-100 hover:border-none">
           Agenda<span className=" text-blue-950">Inmo</span>
         </a>
       </div>
-
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><NavLink to="/" className={({ isActive }) =>
-            isActive ? "font-bold text-primary !bg-gray-50" : " font-semibold hover:text-primary hover:!bg-gray-100"
-          }>Inicio</NavLink></li>
-          <li><NavLink to="/clientes" className={({ isActive }) =>
-            isActive ? "font-bold text-primary !bg-gray-50" : " font-semibold hover:text-primary hover:!bg-gray-100"
-          }>Clientes</NavLink></li>
-          <li><NavLink to="/propiedades" className={({ isActive }) =>
-            isActive ? "font-bold text-primary !bg-gray-50" : " font-semibold hover:text-primary hover:!bg-gray-100"
-          }>Propiedades</NavLink></li>
-          <li><NavLink to="/propiedades-terceros" className={({ isActive }) =>
-            isActive ? "font-bold text-primary !bg-gray-50" : " font-semibold hover:text-primary hover:!bg-gray-100"
-          }>Propiedades Terceros</NavLink></li>
+          <li><NavLink to="/" className={navLinkClasses}>Inicio</NavLink></li>
+          <li><NavLink to="/clientes" className={navLinkClasses}>Clientes</NavLink></li>
+          <li><NavLink to="/propiedades" className={navLinkClasses}>Propiedades</NavLink></li>
+          <li><NavLink to="/propiedades-terceros" className={navLinkClasses}>Propiedades Terceros</NavLink></li>
         </ul>
       </div>
       <div className="navbar-end">
         <a className="btn">Button</a>
       </div>
-
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,58 @@
 import { Link } from "react-router-dom";
+import Hero from "../components/UI/Hero";
 import Card from "../components/UI/Card";
-import { BuildingOffice2Icon, UsersIcon } from "@heroicons/react/24/outline";
-import { HomeIcon } from "@heroicons/react/24/outline";
+import { BuildingOffice2Icon, UsersIcon, HomeIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
-  return (
-  <div>
-      <div className="grid md:grid-cols-3 gap-8 max-w-5xl w-full px-6 mx-auto py-12">
-      <Link to="/clientes">
-        <Card title='Clientes' description='Gestiona tu base de datos de clientes' icon={<div className="bg-blue-100 p-4 rounded-full text-blue-600">
-          <UsersIcon className="h-6 w-6" />
-        </div>}
-          footer={<span className="text-blue-600 font-medium">Acceder →</span>}
-        />
-      </Link>
-      <Link to="/propiedades">
-        <Card title='Propiedades' description="Administra propiedades en venta y alquiler" icon={<div className=" p-4 rounded-full bg-green-100  text-green-600">
-          <HomeIcon className="h-6 w-6" />
-        </div>}
-          footer={<span className="text-green-600 font-medium">Acceder →</span>}
-        />
-      </Link>
-      <Link to='/propiedades-terceros'>
-        <Card title="Propiedades Terceros" description="Gestiona propiedades de otros agentes y colaboraciones" icon={<div className=" bg-purple-100 p-4 rounded-full text-purple-600 ">
-          <BuildingOffice2Icon className="h-6 w-6" />
+  const handleScroll = () => {
+    const section = document.getElementById("cards-section");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-        </div>} footer={<span className="text-purple-600 font-medium">Acceder →</span>}
-        />
-      </Link>
+  return (
+    <div>
+      {/* HERO */}
+      <Hero onScrollToCards={handleScroll} />
+
+      {/* CARDS */}
+      <div id="cards-section" className="grid md:grid-cols-3 gap-8 w-full px-6 py-12">
+        <Link to="/clientes">
+          <Card
+            title="Clientes"
+            description="Gestiona tu base de datos de clientes"
+            icon={<div className="bg-blue-100 p-4 rounded-full text-blue-600">
+              <UsersIcon className="h-6 w-6" />
+            </div>}
+            footer={<span className="text-blue-600 font-medium">Acceder →</span>}
+          />
+        </Link>
+        <Link to="/propiedades">
+          <Card
+            title="Propiedades"
+            description="Administra propiedades en venta y alquiler"
+            icon={<div className="p-4 rounded-full bg-green-100 text-green-600">
+              <HomeIcon className="h-6 w-6" />
+            </div>}
+            footer={<span className="text-green-600 font-medium">Acceder →</span>}
+          />
+        </Link>
+        <Link to="/propiedades-terceros">
+          <Card
+            title="Propiedades Terceros"
+            description="Gestiona propiedades de otros agentes y colaboraciones"
+            icon={<div className="bg-purple-100 p-4 rounded-full text-purple-600">
+              <BuildingOffice2Icon className="h-6 w-6" />
+            </div>}
+            footer={<span className="text-purple-600 font-medium">Acceder →</span>}
+          />
+        </Link>
+      </div>
+
+      <div className="w-80 md:w-1/2 mx-auto">
+        <Card description="Selecciona una sección para comenzar" />
+      </div>
     </div>
-    <div className="w-80 md:w-1/2  mx-auto">
-      <Card description="Selecciona una sección para comenzar"/>
-    </div>
-  </div>
-  )
+  );
 }
